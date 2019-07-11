f384dc40d801c4a6219578b18716fbde7855a943
Jason Underhill <jason@junderhill.com>
2019-07-11 17:32:30 +0100

    Add npgsql reference

--- src/Todos.Infrastructure/ReadDatastore/TodoReadRepository.cs
+++ src/Todos.Infrastructure/ReadDatastore/TodoReadRepository.cs
@@ -2,11 +2,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Todos.Core.Domain;
 using Todos.Core.RepositoryContracts;
+using Npgsql;
 
 namespace Todos.Infrastructure.ReadDatastore
 {
     public class TodoReadRepository : ITodoReadRepository
     {
+        private readonly Npgsql.NpgsqlConnection connection;
+        public TodoReadRepository()
+        {
+            //todo: where to get the db connection from? inject? factory?            
+        }
+
         public Task<IEnumerable<TodoListItem>> GetTodos()
         {
             throw new System.NotImplementedException();
